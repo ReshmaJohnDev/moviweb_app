ad449cda012303d8df1622c60ee0979350147967
Reshma John <reshma.chackola@gmail.com>
2025-03-25 09:36:43 +0100

    Added the abstact methods

--- datamanager/data_manager_interface.py
+++ datamanager/data_manager_interface.py
@@ -9,3 +9,19 @@ class DataManagerInterface(ABC):
     @abstractmethod
     def get_user_movies(self, user_id):
         pass
+
+    @abstractmethod
+    def add_user(self,user_name):
+        pass
+
+    @abstractmethod
+    def add_movie(self, user_id):
+        pass
+
+    @abstractmethod
+    def update_movie(self, user_id, movie_id):
+        pass
+
+    @abstractmethod
+    def delete_movie(self, user_id, movie_id):
+        pass
